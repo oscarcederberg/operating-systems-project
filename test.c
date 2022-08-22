fc1c01567a213dbb3e666109852496fe09544f3c
Oscar Cederberg <os7138ce-s@student.lu.se>
2022-08-22 13:56:32 +0200

    Typo

--- test.c
+++ test.c
@@ -125,7 +125,7 @@ int main() {
     printf("OK\n");
     test_n++;
     
-    printf("%lu: calloc, and freeing %lu times\ntesting... ", test_n, iterations);
+    printf("%lu: calloc,and freeing %lu times\ntesting... ", test_n, iterations);
     for (size_t i = 0; i < iterations; ++i) {
         new_length = 100 * i;
         free(memory_1);
@@ -146,7 +146,7 @@ int main() {
     printf("OK\n");
     test_n++;
     
-    printf("%lu: realloc, and freeing %lu times\ntesting... ", test_n, iterations);
+    printf("%lu: realloc,and freeing %lu times\ntesting... ", test_n, iterations);
     for (size_t i = 0; i < iterations; ++i) {
         new_length = 100 * i;
         memory_1 = realloc(memory_1, new_length * sizeof(unsigned long int));
